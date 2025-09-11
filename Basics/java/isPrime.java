a5655cfff3ee0b57150e2df7bde55bd9c99d0eac
Vamshi9415 <21mcme03@uohyd.ac.in>
2025-09-11 12:14:24 +0530

    Add isPrime.java

---
+++ Basics/java/isPrime.java
@@ -0,0 +1,25 @@
+
+public class isPrime {
+    public static boolean isaPrime(int number){
+
+        if(number <=1){
+            return false;
+        }
+
+        for(int i=2; i<=(int)Math.sqrt(number); i++){
+            if(number%i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+        int num = 29; 
+        if (isaPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
