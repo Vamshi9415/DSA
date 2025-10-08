cf9fa4f3f14bb8f7c72a6b6051083ea8af84a2c7
Vamshi9415 <21mcme03@uohyd.ac.in>
2025-10-09 01:44:44 +0530

    added print 1 to N numbers in recursion

---
+++ Recursion/java/print1toN.java
@@ -0,0 +1,12 @@
+public class print1toN {
+    public static void f(int i, int n){
+        if (i > n){
+            return;
+        }
+        System.out.println(i);
+        f(i+1,n);
+    }
+    public static void main(String args[]){
+        f(1,100);
+    }
+}
